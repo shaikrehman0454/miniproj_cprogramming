86796df14c6a9e25b08d2121a853ce7389d9ab99
shaikrehman0454 <shaikrehaman0454@gmail.com>
2024-12-31 10:50:57 +0530

    Add files via upload

---
+++ mini project.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char operator;
+
+    printf("Basic Calculator\n");
+    printf("Enter first number: ");
+    scanf("%lf", &num1);  
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);  
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {  
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator! Please use one of the following: +, -, *, /\n");
+    }
+
+    return 0;
+}
